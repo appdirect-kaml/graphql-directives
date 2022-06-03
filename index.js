69dc66d175a5a1fcaa69a6008e5d471d3092f5fb
Mohamed Mayallo <mayallo@baianat.com>
2022-06-03 23:07:03 +0200

    Init

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { mapSchema, getDirective, MapperKind } from '@graphql-tools/utils';
 import { defaultFieldResolver, GraphQLObjectType } from 'graphql';
 
-// Our GraphQL schema
 const typeDefs = gql`
   directive @upper on FIELD_DEFINITION
   directive @rest(url: String!) on FIELD_DEFINITION
@@ -29,36 +28,29 @@ const typeDefs = gql`
   }
 `;
 
-// Our resolvers (notice the hard-coded string is *not* all-caps)
 const resolvers = {
   Query: {
     async post(_, args) {
-      return args.post;
+      return args.post; // Injected into `args` by the `@rest` directive
     }
   }
 };
 
-// This function takes in a schema and adds upper-casing logic
-// to every resolver for an object field that has a directive with
-// the specified name (we're using `upper`)
 function upperDirectiveTransformer(schema, directiveName) {
   return mapSchema(schema, {
-    // Executes once for each object field in the schema
     [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
+      // `OBJECT_FIELD` is the mapperkind while `FIELD_DEFINITION` is location name in schema
       // Check whether this field has the specified directive
       const upperDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
-
       if (upperDirective) {
         // Get this field's original resolver
+        // If the original resolver is not given, then a default resolve behavior is used
         const { resolve = defaultFieldResolver } = fieldConfig;
-
         // Replace the original resolver with a function that *first* calls
         // the original resolver, then converts its result to upper case
         fieldConfig.resolve = async function (source, args, context, info) {
-          const result = await resolve(source, args, context, info);
-          if (typeof result === 'string') {
-            return result.toUpperCase();
-          }
+          const result = await resolve(source, args, context, info); // Calling the original resolver
+          if (typeof result === 'string') return result.toUpperCase(); // Uppercasing the result
           return result;
         };
         return fieldConfig;
@@ -71,14 +63,12 @@ function dateDirectiveTransformer(schema, directiveName) {
   return mapSchema(schema, {
     [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
       const dateDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
-
       if (dateDirective) {
         const { resolve = defaultFieldResolver } = fieldConfig;
-        const { format } = dateDirective;
-
-        // hello d
+        const { format } = dateDirective; // Get the directive param
         fieldConfig.resolve = async function (source, args, context, info) {
           const result = await resolve(source, args, context, info);
+					if (!result) return null;
           try {
             return dateFormat(result, format);
           } catch {
@@ -93,16 +83,14 @@ function dateDirectiveTransformer(schema, directiveName) {
 
 function restDirectiveTransformer(schema, directiveName) {
   return mapSchema(schema, {
-    // Executes once for each object field in the schema
     [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
       const restDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
-
       if (restDirective) {
-        const { url } = restDirective;
-
+        const { url } = restDirective; // Get the param
         const { resolve = defaultFieldResolver } = fieldConfig;
         fieldConfig.resolve = async function (source, args, context, info) {
-          let { data } = await axios.get(url);
+          let { data } = await axios.get(url); // Use axios to get the post from a third-party
+          // Inject the post in `args` to be able to return it from resolver
           return await resolve(source, { ...args, post: data }, context, info);
         };
         return fieldConfig;
@@ -113,15 +101,13 @@ function restDirectiveTransformer(schema, directiveName) {
 
 function authDirectiveTransformer(schema, directiveName) {
   return mapSchema(schema, {
-    // Executes once for each object field in the schema
     [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
       const authDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
-
       if (authDirective) {
         const { resolve = defaultFieldResolver } = fieldConfig;
-        const { role } = authDirective;
-
+        const { role } = authDirective; // Get the directive param
         fieldConfig.resolve = async function (source, args, context, info) {
+					// Check the authorization before calling the resolver itself
           if (role !== 'ADMIN') throw new ApolloError('Unauthorized!');
           return await resolve(source, args, context, info);
         };
@@ -133,12 +119,11 @@ function authDirectiveTransformer(schema, directiveName) {
 
 function uuidDirectiveTransformer(schema, directiveName) {
   return mapSchema(schema, {
-    // Executes once for each object field in the schema
+    // The mapper for OBJECT is OBJECT_TYPE
     [MapperKind.OBJECT_TYPE]: (type) => {
       const uuidDirective = getDirective(schema, type, directiveName)?.[0];
-
       if (uuidDirective) {
-        const { field } = uuidDirective;
+        const { field } = uuidDirective; // Get the directive param
         const config = type.toConfig();
         config.fields[field].resolve = () => crypto.randomUUID();
         return new GraphQLObjectType(config);
@@ -149,19 +134,16 @@ function uuidDirectiveTransformer(schema, directiveName) {
 
 function lengthDirectiveTransformer(schema, directiveName) {
   return mapSchema(schema, {
-    // Executes once for each object field in the schema
     [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
       const lengthDirective = getDirective(schema, fieldConfig, directiveName)?.[0];
-
       if (lengthDirective) {
         const { resolve = defaultFieldResolver } = fieldConfig;
         const { min, max } = lengthDirective;
-
         fieldConfig.resolve = async function (source, args, context, info) {
           const result = await resolve(source, args, context, info);
           if (min !== undefined && typeof result === 'string' && result.length < min) {
             throw new ApolloError(
-              `The field ${fieldConfig.astNode.name.value} should contain atleast ${min} characters`
+              `The field ${fieldConfig.astNode.name.value} should contain at least ${min} characters`
             );
           }
           if (max !== undefined && typeof result === 'string' && result.length > max) {
@@ -177,7 +159,6 @@ function lengthDirectiveTransformer(schema, directiveName) {
   });
 }
 
-// Create the base executable schema
 let schema = makeExecutableSchema({
   typeDefs,
   resolvers
@@ -191,7 +172,6 @@ schema = lengthDirectiveTransformer(schema, 'length');
 schema = uuidDirectiveTransformer(schema, 'uuid');
 schema = dateDirectiveTransformer(schema, 'date');
 
-// Provide the schema to the ApolloServer constructor
 const server = new ApolloServer({ schema });
 
 server.listen().then(({ url }) => {
